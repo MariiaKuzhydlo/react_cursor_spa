a008ed80679cae364806cc843c78b5d2ae5f3a7b
Mariia Kuzhydlo <kushnirmariia.m@gmail.com>
2022-07-29 19:38:30 +0300

    hw added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { HashRouter } from "react-router-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import ContactsPage from "./components/ContactsPage";
 import HomePage from './components/HomePage';
@@ -10,12 +11,12 @@ import PostsPage from "./components/PostsPage";
 function App() {
 	return (
 		<div className="App">
-			<Router>
+			<HashRouter>
 				<Route path="/" exact component={HomePage} />
 				<Route path="/posts" component={PostsPage} />
 				<Route path="/photos" component={PhotosPage} />
 				<Route path="/contacts" component={ContactsPage} />
-			</Router>
+			</HashRouter>
 		</div>
 	);
 }
